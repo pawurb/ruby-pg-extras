a74821aa57e5095511e1ec932175437abb965ed8
pawurb <contact@pawelurbanek.com>
2025-12-16 22:59:32 +0100

    Add pg18 to CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,9 +12,10 @@ PG_PORTS = {
   "15" => "5435",
   "16" => "5436",
   "17" => "5437",
+  "18" => "5438",
 }
 
-port = PG_PORTS.fetch(pg_version, "5432")
+port = PG_PORTS.fetch(pg_version, "5438")
 
 ENV["DATABASE_URL"] ||= "postgresql://postgres:secret@localhost:#{port}/ruby-pg-extras-test"
 
